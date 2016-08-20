09d5094c2223b90fef467e2dc1ee7302d2960ee3
ipp-docker-hub-client <ipp-docker-hub-client@users.noreply.github.com>
2016-08-20 21:57:26 +0800

    Update and rename entrypoint.sh to docker-entrypoint.sh

---
+++ docker-entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [[ "$1" == nginx ]] || [ "$1" == php-fpm ]; 
+then
+  echo "Running PHP-FPM ..."
+  php-fpm --allow-to-run-as-root #--nodaemonize can stop run in foreground but we already have nginx in foreground so no issue with docker.
+  echo "Running Nginx ..."
+  nginx -g 'daemon off;'
+else
+ exec "$@"
+fi
+
--- entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-echo "Running PHP-FPM ..."
-php-fpm --allow-to-run-as-root #--nodaemonize can stop run in foreground but we already have nginx in foreground so no issue with docker.
-echo "Running Nginx ..."
-nginx -g 'daemon off;'
